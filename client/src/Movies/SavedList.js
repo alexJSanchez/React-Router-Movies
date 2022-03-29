edab179a61d106a8a545064febe9334f3a7b366b
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-28 21:57:29 -0400

    add useHistory function

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 export default function SavedList(props) {
+ const history = useHistory();
   const goHome = () =>{
     history.push('/');
   }
